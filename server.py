2d845586ba7ad49b50f296ca7ed7086cdd664584
harish5127 <harish18old@gmail.com>
2026-03-14 20:23:56 +0530

    commit

---
+++ server.py
@@ -0,0 +1,9 @@
+import socket
+
+s = socket.socket()
+s.connect(('localhost', 8000))
+
+while True:
+    ip = input("Enter the website you want to ping ")
+    s.send(ip.encode())
+    print(s.recv(1024).decode())
